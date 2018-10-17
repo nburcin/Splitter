fc2b13d177796a8e9273185802f867b8b3d5236a
Nick Burcin <nicholas.burcin@outlook.com>
2018-10-17 08:56:44 -0400

    Added data into arrays and started logic

---
+++ Splitter/Splitter/Images+Names+Colors.swift
@@ -0,0 +1,27 @@
+//
+//  Images+Names+Colors.swift
+//  Splitter
+//
+//  Created by Nicholas Burcin on 10/17/18.
+//  Copyright © 2018 Burcin Software. All rights reserved.
+//
+
+import UIKit
+
+// Images
+let camera = UIImage(named: "1")!
+let city = UIImage(named: "2")!
+let buffalo = UIImage(named: "3")!
+let bouquet = UIImage(named: "4")!
+let stand = UIImage(named: "5")!
+let basketball = UIImage(named: "6")!
+
+let imageArray = [camera, city, buffalo, bouquet, stand, basketball]
+
+// Names
+let nameArray = ["CAMERA", "CITY", "ANIMALS", "FLOWERS", "STAND", "BASKETBALL"]
+
+// Colors
+
+let colorArray = [#colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1), #colorLiteral(red: 0.1019607857, green: 0.2784313858, blue: 0.400000006, alpha: 1), #colorLiteral(red: 0.1215686277, green: 0.01176470611, blue: 0.4235294163, alpha: 1), #colorLiteral(red: 0.5725490451, green: 0, blue: 0.2313725501, alpha: 1), #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1), #colorLiteral(red: 0.9686274529, green: 0.78039217, blue: 0.3450980484, alpha: 1)]
+
